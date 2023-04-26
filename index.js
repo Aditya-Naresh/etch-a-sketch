88b1443264e398c7c64b4523dd9e0d4d4a0936e9
Aditya-Naresh <adityanaresh007@gmail.com>
2023-04-26 22:31:46 +0530

    Size can be changed using prompt now

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 let board = document.querySelector('.board');
 let outerDiv = [];
+let size = 16;
+const myButton = document.getElementById("sizeButton");
+myButton.addEventListener("click", function() {
+  size = parseInt(prompt("Enter the desired size"))
 
-for (let i = 0; i < 16; i++) {
-    outerDiv[i] = document.createElement('div');
-    outerDiv[i].classList.add('row');
+  while (board.firstChild) {
+    board.removeChild(board.firstChild);
+  }
 
-    let innerDiv = [];
-
-    for (let j = 0; j < 16; j++) {
-        innerDiv[j] = document.createElement('div');
-        innerDiv[j].classList.add('box');
-        outerDiv[i].appendChild(innerDiv[j]); 
-        
-        innerDiv[j].addEventListener("mouseenter", () => {
-            innerDiv[j].classList.add('black')
-        })
-
-        innerDiv[j].addEventListener("mouseleave", () => {
-            innerDiv[j].classList.remove('black')
-        })
-    } 
-    board.appendChild(outerDiv[i]);  
-}
+  for (let i = 0; i < size; i++) {
+      outerDiv[i] = document.createElement('div');
+      outerDiv[i].classList.add('row');
+  
+      let innerDiv = [];
+  
+      for (let j = 0; j < size; j++) {
+          innerDiv[j] = document.createElement('div');
+          innerDiv[j].classList.add('box');
+          outerDiv[i].appendChild(innerDiv[j]); 
+          
+          innerDiv[j].addEventListener("mouseenter", () => {
+              innerDiv[j].classList.add('black')
+          })
+  
+          innerDiv[j].addEventListener("mouseleave", () => {
+              innerDiv[j].classList.remove('black')
+          })
+      } 
+      board.appendChild(outerDiv[i]);  
+  }
+});
