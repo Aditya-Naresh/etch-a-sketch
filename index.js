9fe86e2477c91f42bede8a00895c2aff06c3105f
Aditya-Naresh <adityanaresh007@gmail.com>
2023-04-26 22:10:48 +0530

    MouseEnter and Leaving trails added

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         innerDiv[j] = document.createElement('div');
         innerDiv[j].classList.add('box');
-        outerDiv[i].appendChild(innerDiv[j]);  
+        outerDiv[i].appendChild(innerDiv[j]); 
+        
+        innerDiv[j].addEventListener("mouseenter", () => {
+            innerDiv[j].classList.add('black')
+        })
+
+        innerDiv[j].addEventListener("mouseleave", () => {
+            innerDiv[j].classList.remove('black')
+        })
     } 
     board.appendChild(outerDiv[i]);  
 }
